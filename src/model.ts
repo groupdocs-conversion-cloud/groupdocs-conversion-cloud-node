e1b5af4e836b376178e31732083ad16037b72197
product-team <product.team@groupdocs.com>
2025-04-22 01:02:18 -0500

    Updated sources

--- src/model.ts
+++ src/model.ts
@@ -132,6 +132,11 @@ export class ConsumptionResult {
             name: "quantity",
             baseName: "quantity",
             type: "number",
+        },        
+        {
+            name: "billedApiCalls",
+            baseName: "billedApiCalls",
+            type: "number",
         }    ];
 
     /**
@@ -150,6 +155,11 @@ export class ConsumptionResult {
      * Amount of MBs processed
      */
     public quantity: number;
+    
+    /**
+     * Billed API calls number
+     */
+    public billedApiCalls: number;
     
     public constructor(init?: Partial<ConsumptionResult>) {
         
